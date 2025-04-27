1b83b90869b63e1659f13e5a410e6fd230b2c958
samik <samikshadissanayake@gmail.com>
2025-04-28 00:15:15 +0530

    Fix some errors in comment function

--- frontend/src/Pages/PostManagement/AddNewPost.js
+++ frontend/src/Pages/PostManagement/AddNewPost.js
@@ -73,7 +73,6 @@ function AddNewPost() {
     const updatedMedia = [...media];
     const updatedPreviews = [...mediaPreviews];
     
-    // Revoke the object URL to prevent memory leaks
     URL.revokeObjectURL(mediaPreviews[index].url);
     
     updatedMedia.splice(index, 1);
