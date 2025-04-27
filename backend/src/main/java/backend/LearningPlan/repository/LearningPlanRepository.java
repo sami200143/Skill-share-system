309353b55f2864dd41936f510d80097885243e26
samik <samikshadissanayake@gmail.com>
2025-04-28 00:18:43 +0530

    Implement api for comment functions

---
+++ backend/src/main/java/backend/LearningPlan/repository/LearningPlanRepository.java
@@ -0,0 +1,11 @@
+package backend.LearningPlan.repository;
+
+import backend.LearningPlan.model.LearningPlanModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface LearningPlanRepository extends MongoRepository<LearningPlanModel, String> {
+    void deleteByPostOwnerID(String postOwnerID);
+    List<LearningPlanModel> findByPostOwnerID(String postOwnerID);
+    List<LearningPlanModel> findByPostOwnerName(String postOwnerName); // New method
+}
