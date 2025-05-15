63551f6ea24fc152428cc308b36d941c2f402fcd
saku1215 <sakusewwandi09@gmail.com>
2025-05-15 20:45:56 +0530

    Add getters and setters

--- backend/src/main/java/backend/Achievements/model/AchievementsModel.java
+++ backend/src/main/java/backend/Achievements/model/AchievementsModel.java
@@ -1,8 +1,9 @@
 package backend.Achievements.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "Achievements")
 public class AchievementsModel {
@@ -30,3 +31,67 @@ public class AchievementsModel {
         this.category = category;
         this.imageUrl = imageUrl;
     }
+
+        public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPostOwnerID() {
+        return postOwnerID;
+    }
+
+    public void setPostOwnerID(String postOwnerID) {
+        this.postOwnerID = postOwnerID;
+    }
+
+    public String getPostOwnerName() {
+        return postOwnerName;
+    }
+
+    public void setPostOwnerName(String postOwnerName) {
+        this.postOwnerName = postOwnerName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
