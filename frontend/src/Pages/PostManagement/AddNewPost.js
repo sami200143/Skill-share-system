a23d95030ca2fcf68671040c3d8b559a74894818
SThathsara-u <thathsara1221@gmail.com>
2025-05-15 20:54:00 +0530

    Ipmort React

--- frontend/src/Pages/PostManagement/AddNewPost.js
+++ frontend/src/Pages/PostManagement/AddNewPost.js
@@ -1,3 +1,8 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import NavBar from '../../Components/NavBar/NavBar';
+import './AddNewPost.css'; // Make sure to include the CSS filee
+
 //create function for add post
 function AddNewPost() {
   const [title, setTitle] = useState('');
