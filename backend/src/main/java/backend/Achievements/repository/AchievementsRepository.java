9726f716f64a9e96e006436c24c223f3a91618c1
saku1215 <sakusewwandi09@gmail.com>
2025-05-15 20:49:12 +0530

    Add achievement repo interface with custom delete method

--- backend/src/main/java/backend/Achievements/repository/AchievementsRepository.java
+++ backend/src/main/java/backend/Achievements/repository/AchievementsRepository.java
@@ -1,8 +1,10 @@
 package backend.Achievements.repository;
 
-import backend.Achievements.model.AchievementsModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import backend.Achievements.model.AchievementsModel;
 
 public interface AchievementsRepository extends MongoRepository<AchievementsModel, String> {
     void deleteByPostOwnerID(String postOwnerID); // Ensure this method exists
 }
+
